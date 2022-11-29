444757c16efb6d833a29741aef955a34f44b8c26
Henrique Araujo <henrique.araujo2000@gmail.com>
2022-11-29 18:26:32 -0300

    Update R.Graph

--- RGraphs.R
+++ RGraphs.R
@@ -1,44 +1,51 @@
 #Importing and installing ggplot library
 install.packages('tidyverse')
 library(tidyverse)
+library(ggplot2)
+library(dplyr)
 
-Molecules <- read.csv("C:/Users/mathe/OneDrive/Área de Trabalho/GitHub/Zinc12-description/File_name.csv", header = T, sep=";")
-names(Molecules)
+#Reading the csv file and printing it
+Molecules <- read.csv("File_name_real.csv", header = T, sep=";")
+Molecules
 
-#Análise exploratória do numero de átomos 
+
+#Exploratory analysis of the Number of Atoms
 boxplot(Molecules$NumAtoms)
-mean(Molecules$NumAtoms)
-median(Molecules$NumAtoms)
-hist(Molecules$NumAtoms)
+mean(Molecules$NumAtoms) #mean = 21.01
+median(Molecules$NumAtoms) #median = 21
+hist(Molecules$NumAtoms) #histogram
 
-#Análise exploratória do peso molecular
+
+#Exploratory analysis of Molecular Weight
 boxplot(Molecules$MolWt)
-mean(Molecules$MolWt)
-median(Molecules$MolWt)
+mean(Molecules$MolWt) #mean = 300.88
+median(Molecules$MolWt) #median = 301.14
 hist(Molecules$MolWt)
 
-#Anális explotatória do TPSA
+
+#Exploratory analysis of TPSA
 boxplot(Molecules$TPSA)
+mean(Molecules$TPSA) #mean = 67.45
+median(Molecules$TPSA) #median = 66.04
 hist(Molecules$TPSA)
-mean(Molecules$TPSA)
-median(Molecules$TPSA)
 
-#Anális3 explotatória do MolLogp
+
+#Exploratory analysis of MolLogp
 boxplot(Molecules$MolLogp)
+mean(Molecules$MolLogp) #mean = 2.28
+median(Molecules$MolLogp) #median = 2.48
 hist(Molecules$MolLogp)
-mean(Molecules$MolLogp)
-median(Molecules$MolLogp)
-
-#Anális3 explotatória do RingCount
 
 
+#Exploratory analysis of RingCount
+#Creating graphic to better understand the correlation
 ggplot(data=Molecules) + geom_point(mapping = aes(x=TPSA, y=HDonors, colour=HAcceptors)) + geom_smooth(mapping = aes(x=TPSA, y=HDonors))
 cor.test(x=Molecules$TPSA, y=Molecules$HDonors, method = "pearson")
-#0.3 (Correlação positiva fraca)
+#0.3 (Weak positive correlation)
 
 ggplot(data=Molecules) + geom_point(mapping = aes(x=TPSA, y=HAcceptors, colour=HDonors)) + geom_smooth(mapping = aes(x=TPSA, y=HAcceptors))
 cor.test(x=Molecules$TPSA, y=Molecules$HAcceptors, method = "pearson")
-#0.68 (Correlação positiva forte)
+#0.68 (Strong positive correlation)
 
 ggplot(data=Molecules) + geom_point(mapping = aes(x=TPSA, y=RotableBonds))
 
@@ -50,20 +57,23 @@ ggplot(data=Molecules) + geom_point(mapping = aes(x=MolWt, y=NumAtoms, color=HAc
 
 ggplot(data=Molecules) + geom_point(mapping = aes(x=TPSA, y=MolLogp, colour=HDonors)) + geom_smooth(mapping = aes(x=TPSA, y=MolLogp))
 cor.test(x=Molecules$TPSA, y=Molecules$MolLogp, method = "pearson")
-# -0.41 (Correlação negativa média)
+# -0.41 (Negative average correlation)
 
-#Descobrindo quais são as moleculas mais polares
+
+#Finding out which molecules are more polar
 index <- sort(Molecules$TPSA, decreasing = TRUE)
 Molecules$ZINC_ID[index]
 # Top 10
 # "ZINC00000913" "ZINC00000913" "ZINC00000913" "ZINC00000869" "ZINC00000869" 
 # "ZINC00000869" "ZINC00000869" "ZINC00000869" "ZINC00000869" "ZINC00000869"
 
-#Descobrindo quais são as moleculas mais apolares
+
+#Finding out which molecules are more apolar/ less polar
 index <- sort(Molecules$HAcceptors, decreasing = TRUE)
 index
 Molecules$ZINC_ID[index]
 
-#Selecionando o ID das mol com mais regiões aceptoras de H
+
+#Selecting the ID of molecules that have regions with better Hydrogen acceptors
 HighHAccept <- filter(Molecules, HAcceptors >= 10)
 select(HighHAccept, ZINC_ID)
